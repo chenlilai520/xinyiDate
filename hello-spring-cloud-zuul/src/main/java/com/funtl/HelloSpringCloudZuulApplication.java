33a0fa43aceb1205704854ea2646e090582e1d8c
cll <724129520@qq.com>
2019-02-20 14:23:44 +0800

    仪表盘

---
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/HelloSpringCloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.funtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudZuulApplication.class, args);
+    }
+
+}
