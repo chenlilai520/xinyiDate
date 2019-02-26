54d22e83477e9a4d9d39250995f034e06c9c3d87
cll <724129520@qq.com>
2019-02-26 14:50:18 +0800

    hah

---
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/HelloSpringCloudZuulApplication.java
@@ -0,0 +1,17 @@
+package com.funtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class HelloSpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudZuulApplication.class, args);
+    }
+
+}
