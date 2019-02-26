230031a2ef341608d30b53848889da70bdd74c15
cll <724129520@qq.com>
2019-02-26 15:17:59 +0800

    分布式配置中心服务端

---
+++ hello-spring-cloud-config/src/main/java/com/funtl/HelloSpringCloudConfigApplication.java
@@ -0,0 +1,17 @@
+package com.funtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class HelloSpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudConfigApplication.class, args);
+    }
+
+}
